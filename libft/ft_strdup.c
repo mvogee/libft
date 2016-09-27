a0a36e0016c28e3c32a9273aca642a2d3c4f455d
Matthew Vogee <mvogee@yahoo.com>
2016-09-26 23:08:49 -0700

    file backup

--- libft/ft_strdup.c
+++ libft/ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: mvogee <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/09/22 13:26:39 by mvogee            #+#    #+#             */
-/*   Updated: 2016/09/26 10:44:49 by mvogee           ###   ########.fr       */
+/*   Updated: 2016/09/26 22:37:13 by mvogee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,9 @@
 
 char	*ft_strdup(const char *s1)
 {
-	int		count;
 	int		strlen;
 	char	*s2;
 
-	count = 0;
-	if (!s1)
-		return (NULL);
 	strlen = ft_strlen((char*)(s1));
 	s2 = (char*)malloc(sizeof(char) * (strlen + 1));
 	if (!s2)
