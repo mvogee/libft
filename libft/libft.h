de2cb78d1a73a73f4a352fa611862379b29b6018
Matthew Vogee <mvogee@yahoo.com>
2016-09-27 11:07:24 -0700

    addition files

--- libft/libft.h
+++ libft/libft.h
@@ -43,6 +43,7 @@ void			*ft_memchr(const void *s, int c, size_t n);
 size_t			ft_strlcat(char *dst, const char *src, size_t size);
 int				ft_atoi(const char *str);
 size_t			ft_strlcat(char *dst, const char *src, size_t size);
+void			*ft_memccpy(void *dst, const void *src, int c, size_t n);
 
 
 char			*ft_putstr(char *str);
